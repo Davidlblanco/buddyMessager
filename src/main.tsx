be4cf5d2eefec73f4bc79855008f0957fb539c8a
David Blanco <david.lucas@maeztra.com>
2025-02-28 15:42:30 -0300

    roll back

--- src/main.tsx
+++ src/main.tsx
@@ -2,26 +2,12 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
-import {
-    // BrowserRouter,
-    createBrowserRouter,
-    RouterProvider,
-} from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
-const router = createBrowserRouter([
-    {
-        path: '/buddyMessager',
-        element: <App />,
-    },
-    {
-        path: '/',
-        element: <App />,
-    },
-]);
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        {/* <BrowserRouter> */}
-        <RouterProvider router={router} />
-        {/* </BrowserRouter> */}
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
     </StrictMode>,
 );
