2b93667198ec759d31030b8707d01a84bd3556d5
David Blanco <david.lucas@maeztra.com>
2025-02-28 15:37:46 -0300

    new routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,27 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import { BrowserRouter } from 'react-router-dom'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.tsx';
+import {
+    // BrowserRouter,
+    createBrowserRouter,
+    RouterProvider,
+} from 'react-router-dom';
 
+const router = createBrowserRouter([
+    {
+        path: '/buddyMessager',
+        element: <App />,
+    },
+    {
+        path: '/',
+        element: <App />,
+    },
+]);
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>,
-)
+    <StrictMode>
+        {/* <BrowserRouter> */}
+        <RouterProvider router={router} />
+        {/* </BrowserRouter> */}
+    </StrictMode>,
+);
